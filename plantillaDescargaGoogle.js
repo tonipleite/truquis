3b7a459f0e4aa55cbd59df82feedd55c519a46e9
tonipleite <tonipleite@users.noreply.github.com>
2020-06-18 12:17:22 +0200

    Versión final de la plantilla google

--- plantillaDescargaGoogle.js
+++ plantillaDescargaGoogle.js
@@ -1,67 +1,65 @@
-var the_list = [
+// VARIBALES GLOBALES
+var base_time = 500; // Minimum wait time
+var increment = 500; // Amount of time to be incremented by
+var max_multiplier=15; // Maximum multiplier to be hold
+var success_min_count = 5; // Minimum amount of succesful rounds to consider decreasing the multiplier
+var success_count = 0; // Number of continued succesful rounds
+var multiplier = 0; //multiplier of increment
+var counter=0;  // General round counter
+var out_list = [];
+var in_list = [ // Array where to provide the input
     "https://www.google.es/maps/dir/38.2645700,-5.86491000/38.2597000,-5.70195000",
     "https://www.google.es/maps/dir/38.2645700,-5.86491000/38.0286500,-6.21951000",
     "https://www.google.es/maps/dir/38.2645700,-5.86491000/38.2755220,-5.40813800",
     "https://www.google.es/maps/dir/38.6871500,-6.41911000/38.9378600,-6.36065000",
-    "https://www.google.es/maps/dir/38.6871500,-6.41911000/38.8594600,-6.36509000",
-    "https://www.google.es/maps/dir/38.6871500,-6.41911000/38.9385000,-6.36552000",
-    "https://www.google.es/maps/dir/38.2597000,-5.70195000/38.2755220,-5.40813800",
-    "https://www.google.es/maps/dir/38.2597000,-5.70195000/38.2127900,-5.14101000",
-    "https://www.google.es/maps/dir/38.8787700,-6.94602000/38.8847100,-7.02224000",
-    "https://www.google.es/maps/dir/38.8787700,-6.94602000/38.8836110,-6.99735900",
-    "https://www.google.es/maps/dir/38.8847100,-7.02224000/38.8836110,-6.99735900",
-    "https://www.google.es/maps/dir/39.0514000,-5.64921000/39.1410200,-5.93146000",
-    "https://www.google.es/maps/dir/39.0514000,-5.64921000/39.4111900,-5.89079000",
-    "https://www.google.es/maps/dir/38.0286500,-6.21951000/37.9043200,-6.22098000",
-    "https://www.google.es/maps/dir/38.9378600,-6.36065000/38.8594600,-6.36509000",
-    "https://www.google.es/maps/dir/38.9378600,-6.36065000/38.9385000,-6.36552000",
-    "https://www.google.es/maps/dir/38.9378600,-6.36065000/39.1623900,-6.26421000",
-    "https://www.google.es/maps/dir/38.9378600,-6.36065000/39.3824800,-6.34811000",
-    "https://www.google.es/maps/dir/38.9378600,-6.36065000/39.1410200,-5.93146000",
-    "https://www.google.es/maps/dir/38.8594600,-6.36509000/38.9385000,-6.36552000"
+    "https://www.google.es/maps/dir/38.6871500,-6.41911000/38.8594600,-6.36509000"
     ];
 
-// VARIBALES GLOBALES
-var base_time = 500; // Es el tiempo mínimo de espera
-var increment = 500; //
-var max_multiplier=8; //
-var success_min_count = 5; // Conteo de éxitos mínimo para reducir nivel
-var success_count = 0;
-var multiplier = 0; //multiplier de base_time
-var counter=0;  // Empieza aquí (cero por defecto)
-
 function solve (){
-    if (counter < the_list.length){
+    if (counter < in_list.length){
         if (success_count >= success_min_count){
             if (multiplier > 0){
                 multiplier--;
-                console.log("Se reduce el multiplier a " + multiplier);
+                success_count=0;
+                console.log("Multiplier reduced by 1 to " + multiplier);
             }
         }
-        var potato = window.open(the_list[counter]); 
+        var new_window = window.open(in_list[counter]); 
         setTimeout(	function(){ 
                         try{
-                            console.log("ID:" + counter + "ÑÑ" + "URL:" + the_list[counter] + "ÑÑ" + "TITLE:" + potato.document.title + "ÑÑ" + "INFO:" + potato.document.getElementsByClassName("section-directions-trip-numbers")[0].textContent + "ÑÑ");
+                            //console.log("ID:" + counter + "ÑÑ" + "URL:" + in_list[counter] + "ÑÑ" + "TITLE:" + new_window.document.title + "ÑÑ" + "INFO:" + new_window.document.getElementsByClassName("section-directions-trip-numbers")[0].textContent + "ÑÑ");
+                            out_list [counter] = counter + ";\"" +  in_list[counter] + "\";\""  + new_window.document.title + "\";\""  + new_window.document.getElementsByClassName("section-directions-trip-numbers")[0].textContent + "\""; 
                         }catch(err){
+                            success_count=0;
                             if (multiplier < max_multiplier){
                                 multiplier++;
                                 t_new=base_time + multiplier *increment;
                                 console.log(err.message + " - se incrementa el tiempo a " + t_new + " con el multiplier a " + multiplier);
+                            }else{
+                                console.log(err.message + " - no se puede incrementar el tiempo.")
                             }
                             counter--;
                         }
-                        potato.close();
+                        new_window.close();
                         counter++;
                         solve();
                     }
-                    , base_time + multiplier *increment
+                    , base_time + multiplier *increment // The waiting time for the next round
         );
     };	
-    if (counter == the_list.length){
+    if (counter == in_list.length){ // When rounds are finished
         console.log("");
         console.log("");
-        console.log("FINISHED!");
+        console.log("Source retrieval concluded!");
         console.log("");
+        console.log("Printing result in new window...")
+        var new_window = window.open();
+        new_window.document.writeln("<html lang='es'><head><meta charset='utf-8'></head><body>");
+        new_window.document.writeln("ID;URL;TITLE;INFO<br/>");
+        for (i = 0; i < out_list.length; i++) {
+            new_window.document.writeln(out_list[i] + "<br/>");
+        }
+        new_window.document.writeln("</body></html>");
     };
 };
 solve();
